b20ceda57f700e207c1637f7a4619759a6408848
Tatiana-Juin <tatiana.juinp15@gmail.com>
2024-02-24 23:17:15 +0100

    Modification du code pour créer une key unique

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,15 @@ import CardData from './Components/Card'
 
 import './App.css'
 
+// variable pour que les key de la liste soit unique 
+let nextId=0;
+
 function App() {
-  
     //Pour récupérer la valeur 
     const [dataValue,setDataValue] = useState('');
     // Pour ajouter la tache au tableau
     const [tasks,setTasks] = useState([]);
 
-    // Compteur pour créer un identifiant unique 
-    // const [uniqueId,setUniqueId] = useState(0);
-
-    
-    
     // Function pour récupérer la valeur saisie 
     function handleValue(e){
       setDataValue(e.target.value);
@@ -28,24 +25,23 @@ function App() {
       // Pour afficher la valeur du card quand c'est different de vide 
       if(dataValue.trim() !=""){
         // Ajouter une valeur au tableau
-        setTasks([...tasks,dataValue]);
+        setTasks([
+          ...tasks,
+          {id:nextId++,name:dataValue}
+        ]);
       // Initialiser la valeur dans le bouton input ( champs de texte )
         setDataValue('');
       }
       
     }
-    // POUR LE COMPTEUR 
-    // function handleUniqueKey(){
-    //   setUniqueId(uniqueId => uniqueId+1);
-    //   return `${uniqueId}`;
-    // }
+    
 
     return (
       <>
         {/* POUR AFFICHER LE FORMULAIRE */}
-        <form onSubmit={handleTask}>
-          <input type="text" onChange={handleValue} value={dataValue}  />
-          <button >Créer une tache</button>
+        <form >
+            <input type="text" onChange={handleValue}  />
+            <button onClick={handleTask}>Envoyer</button>
         </form>
 
         {/* APPELLE DU COMPOSANT CardData  */}
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,20 +4,15 @@ import { useState } from 'react';
 
 export default function CardData({datas}) {
 
-  // SUPPRIMER
-  const [deleteValue,setDeleteValue] = useState(datas);
-
   
-  function handleDelete(){
-    // POUR SUPPRIMER UNE VALEUR 
-  }
+
   return (
     <>
     {/* AFFICHER TOUS LES ELEMENTS DU TABLEAU AINSI QUE LE BOUTON MODIFIER ET SUPPRIMER  */}
         <ul>
             {datas.map(
               data=>
-                <li key={data}>{data} <button>Modifier</button> <button onClick={handleDelete}>Supprimer</button>  </li>
+                <li key={data.id}>{data.name} <button>Modifier</button> <button >Supprimer</button>  </li>
                
               )}
         </ul>
